00592ae577282f2c98fd7d353f8f1c6ccaa93b63
Jong Hoon Kim <kim532@wisc.edu>
2019-05-03 19:11:39 -0500

    Add tweet scraping functionality

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import csv
 import tweepy
 import config
 
@@ -11,14 +12,31 @@ def authenticate():
     try:
         auth = tweepy.AppAuthHandler(
             config.consumer_key, config.consumer_secret)
-        print("Authentication successfull.")
-        return auth
+        print("Authentication successful.")
+        api = tweepy.API(auth)
+
+        return api
     except tweepy.TweepError:
         print(tweepy.TweepError)
 
 
+# open a csv file and scrape to it
+def scrape(api):
+    # open csv file
+    with open("scrape_data.csv", "a") as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        for tweet in tweepy.Cursor(api.search,
+                                   q=config.query,
+                                   lang="en",
+                                   since=config.start_date,
+                                   until=config.end_date).items(100):
+            print(tweet.text)
+
+
 def main():
-    auth = authenticate()
+    api = authenticate()
+    scrape(api)
 
 
 if __name__ == "__main__":
